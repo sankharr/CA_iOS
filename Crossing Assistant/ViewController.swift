62a16d5ec17917469edd8dc3378dc64a2d14df33
user149049 <user149049@mta7.xtreemnewsmail.com>
2020-12-26 10:27:21 +0530

    Initial Commit

---
+++ Crossing Assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crossing Assistant
+//
+//  Created by user149049 on 12/26/20.
+//  Copyright © 2020 SRR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Crossing AssistantTests/Crossing_AssistantTests.swift
@@ -0,0 +1,34 @@
+//
+//  Crossing_AssistantTests.swift
+//  Crossing AssistantTests
+//
+//  Created by user149049 on 12/26/20.
+//  Copyright © 2020 SRR. All rights reserved.
+//
+
+import XCTest
+@testable import Crossing_Assistant
+
+class Crossing_AssistantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
